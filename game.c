7cbb360713909e66521fbdc41ef607ca0680dc50
francisco <francisc.barraguirre@protonmail.com>
2021-05-28 23:10:23 -0300

    Implemented MOVE and ADD for all directions
    Implemented a crude `undo()` functionality that I hope works for now.
    
    Deleted the `recursive_move_values()` function. I had left it lying
    around in case the iterative approach didn't work.

--- game.c
+++ game.c
@@ -14,6 +14,7 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 //#include <time.h>
 #include <ncurses.h>
 
@@ -29,9 +30,10 @@
 
 
 int (*game_board)[MATRIX_COLS];
+int (*previous_game_board)[MATRIX_COLS]; // use this to implement an undo function
 
-// use this to implement an undo function
-// int (*previous_game_board)[MATRIX_COLS];
+uint32_t game_score = 0;
+uint16_t game_board_tile_highest = 0;
 
 int main() {
 
@@ -47,28 +49,25 @@ int main() {
 
     // the scoreboard has to be a square matrix (n x n), otherwise malloc panics
     game_board = malloc(sizeof(*game_board) * MATRIX_ROWS);
-    // previous_game_board = malloc(sizeof(*game_board) * MATRIX_ROWS);
+    previous_game_board = malloc(sizeof(*previous_game_board) * MATRIX_ROWS);
 
     game_board_spawn_new_values_random(*game_board, MATRIX_COLS, MATRIX_ROWS);
 
     game_board_print(*game_board, MATRIX_COLS, MATRIX_ROWS);
 
-    game_board_tiles_move_direction(KEY_UP, *game_board, MATRIX_COLS, MATRIX_ROWS);
+    game_board_update(KEY_UP, *game_board, *previous_game_board, MATRIX_COLS, MATRIX_ROWS);
     game_board_print(*game_board, MATRIX_COLS, MATRIX_ROWS);
 
-    game_board_tiles_move_direction(KEY_DOWN, *game_board, MATRIX_COLS, MATRIX_ROWS);
+    game_board_update(KEY_DOWN, *game_board, *previous_game_board, MATRIX_COLS, MATRIX_ROWS);
     game_board_print(*game_board, MATRIX_COLS, MATRIX_ROWS);
 
-    game_board_tiles_move_direction(KEY_LEFT, *game_board, MATRIX_COLS, MATRIX_ROWS);
+    game_board_update(KEY_LEFT, *game_board, *previous_game_board, MATRIX_COLS, MATRIX_ROWS);
     game_board_print(*game_board, MATRIX_COLS, MATRIX_ROWS);
 
-    game_board_tiles_move_direction(KEY_RIGHT, *game_board, MATRIX_COLS, MATRIX_ROWS);
+    game_board_update(KEY_RIGHT, *game_board, *previous_game_board, MATRIX_COLS, MATRIX_ROWS);
     game_board_print(*game_board, MATRIX_COLS, MATRIX_ROWS);
-
     //ch = getch();
 
-    //game_board_update(KEY_UP, *game_board, MATRIX_COLS, MATRIX_ROWS);
-    //game_board_update(KEY_DOWN, *game_board, MATRIX_COLS, MATRIX_ROWS);
     //refresh();
 
     // game_board_print(*game_board, COLS, ROWS);
@@ -83,36 +82,31 @@ int main() {
 
 
 // TODO: copy the game_board into previous_game_board before updating game_board
-void game_board_update(int direction, int *arr, uint8_t columns, uint8_t rows) {
+void game_board_update(int direction, int *arr, int *prev_arr, uint8_t columns, uint8_t rows) {
 
 
-    if (direction == KEY_UP) {
-
-
-    }
-
-    else if (direction == KEY_DOWN) {
-
-
-    }
-
-    else if (direction == KEY_LEFT) {
-
-    }
-
-    else if (direction == KEY_RIGHT) {
-
-    }
-
-    else {
+    if (direction != KEY_UP &&
+        direction != KEY_DOWN &&
+        direction != KEY_LEFT &&
+        direction != KEY_RIGHT) {
 
         printf("character not recognized: %d\n", direction);
         // we want to return early in case of an error
         return;
     }
 
-    game_board_spawn_new_values_random(*game_board, columns, rows);
-    game_board_print(*game_board, columns, rows);
+    else {
+
+        // we use memmove instead of memcpy in case these 2 objects overlap in memory
+        memmove(prev_arr, arr, (columns * rows));
+
+        game_board_tiles_move_direction(direction, arr, columns, rows);
+        game_board_print(arr, columns, rows);
+        game_board_tiles_add_pairs(direction, arr, columns, rows);
+        game_board_print(arr, columns, rows);
+        game_board_spawn_new_values_random(*game_board, columns, rows);
+
+    }
 }
 
 
@@ -127,14 +121,10 @@ void game_board_tiles_move_direction(int direction, int *arr, uint8_t columns, u
             // to move one column at a time, we step 4 values since we have a 4x4 array
             for (uint8_t j = i; j < columns * rows; j += columns) {
 
-                printf("value of i: %d, value of j: %d, arr[j]: %d\n", i, j, arr[j]);
-
                 if (arr[j] == 0) {
 
                     for (uint8_t k = j + columns; k < columns * rows; k += columns) {
-                        // printf("k: %d\n", k);
                         if (arr[k] != 0) {
-                            // printf("value of k: %d, value of i: %d, arr[k]: %d\n", k, i, arr[k]);
                             arr[j] = arr[k];
                             arr[k] = 0;
                             break;
@@ -151,21 +141,14 @@ void game_board_tiles_move_direction(int direction, int *arr, uint8_t columns, u
         // we step through the rows 1 at a time
         for (int8_t i = rows - 1; i >= 0; i--) {
 
-            printf("i: %d\n", i);
-
             // to move one column at a time, we step 4 values since we have a 4x4 array
             // we start from the bottom-left corner of the array, because I find it easier to think about.
             for (int8_t j = ((columns * rows) - i) - 1; j >= i; j -= columns) {
-                printf("j: %d\n", j);
-
-                printf("value of i: %d, value of j: %d, arr[j]: %d\n", i, j, arr[j]);
 
                 if (arr[j] == 0) {
 
                     for (int8_t k = j - columns; k >= 0; k -= columns) {
-                        printf("k: %d\n", k);
                         if (arr[k] != 0) {
-                            printf("value of j: %d, value of k: %d, arr[k]: %d\n", j, k, arr[k]);
                             arr[j] = arr[k];
                             arr[k] = 0;
                             break;
@@ -180,17 +163,13 @@ void game_board_tiles_move_direction(int direction, int *arr, uint8_t columns, u
 
         // we step through the rows one at a time, so we step 4 values forward
         for (int8_t i = 0; i <= ((columns * rows) - columns); i += columns) {
-            printf("i: %d\n", i);
 
             for (int8_t j = 0; j < rows; j++) {
 
                 if (arr[i + j] == 0) {
-                    printf("i: %d, j: %d, arr[i + j]: %d\n", i, j, arr[i + j]);
 
                     for (int8_t k = j + 1; k < rows; k++) {
-                        printf("k: %d\n", k);
                         if (arr[i + k] != 0) {
-                            printf("value of i: %d, value of k: %d, arr[k]: %d\n", i, k, arr[i + k]);
                             arr[i + j] = arr[i + k];
                             arr[i + k] = 0;
                             break;
@@ -205,18 +184,15 @@ void game_board_tiles_move_direction(int direction, int *arr, uint8_t columns, u
 
         // we step through the rows one at a time, so we step 4 values forward, starting from the right
         for (int8_t i = rows - 1; i < columns * rows; i += columns) {
-            printf("i: %d\n", i);
 
             // we start from the right of the game board
-            for (int8_t j = 0; j < rows; j++) {
+            for (int8_t j = 0; j < rows - 1; j++) {
 
                 if (arr[i - j] == 0) {
 
                     for (int8_t k = j + 1; k < rows; k++) {
-                        printf("k: %d\n", k);
 
                         if (arr[i - k] != 0) {
-                            printf("value of i: %d, value of k: %d, arr[k]: %d\n", i, k, arr[i + k]);
                             arr[i - j] = arr[i - k];
                             arr[i - k] = 0;
                             break;
@@ -231,6 +207,7 @@ void game_board_tiles_move_direction(int direction, int *arr, uint8_t columns, u
     else {
 
         printf("character not recognized: %d\n", direction);
+        return;
     }
 
 }
@@ -238,69 +215,83 @@ void game_board_tiles_move_direction(int direction, int *arr, uint8_t columns, u
 // TODO: implement UP, DOWN, LEFT and RIGHT directions
 void game_board_tiles_add_pairs(int direction, int *arr, uint8_t columns, uint8_t rows) {
 
-}
 
-
-// TODO: implement DOWN, LEFT and RIGHT directions
-void game_board_recursive_add_pairs(int direction, int counter, int *arr, uint8_t columns, uint8_t rows) {
-
-    printf("recursively adding pairs\n");
-    int k = counter;
-    printf("k: %d, counter: %d\n", k, counter);
+    printf("adding pairs\n");
 
     if (direction == KEY_UP) {
 
-        // we check manually if we're at the end of the array
-        if (k >= columns * rows) {
-            printf("if where k >= cols * rows. k: %d\n", k);
-            return;
+        // we add tiles upwards, so we step through the rows 1 at a time
+        for (uint8_t i = 0; i < rows; i++) {
+            // to move one column at a time, we step 4 values since we have a 4x4 array
+            for (uint8_t j = i; j < columns * rows; j += columns) {
+
+                if (arr[j] != 0 && arr[j] == arr[j + columns]) {
+                    // multiplying by 2 is faster than accessing memory like arr[j] = arr[j + columns]
+                    arr[j] *= 2;
+                    arr[j + columns] = 0;
+                }
+            }
         }
-
-        // we don't want to end up outside of the array
-        else if (k + columns >= columns * rows) {
-            printf("if where k + columns >= cols * rows. k + columns: %d\n", k + columns);
-            return;
-        }
-
-        if (arr[k] == arr[k + columns] && arr[k] != 0) {
-
-            printf("second if where arr[k] == arr[k + cols]\n");
-            printf("k: %d, cols: %d, arr[k] = %d and arr[k + cols] = %d\n", k, columns, arr[k], arr[k + columns]);
-            arr[k] *= 2;
-            arr[k + columns] = 0;
-            // now we need to move the values again because of the new zeroes
-            game_board_recursive_move_values(direction, counter + columns, arr, columns, rows);
-        }
-
-        game_board_recursive_add_pairs(direction, counter + columns, arr, columns, rows);
     }
 
     else if (direction == KEY_DOWN) {
 
-        // we check manually if we're at the __start__ since we began at the bottom-right (the end)
-        if (k < 0) {
-            printf("if where k < 0. k: %d\n", k);
-            return;
-        }
+        // we add tiles downwards, so we step through the rows 1 at a time
+        for (int8_t i = rows - 1; i > 0; i--) {
+            // to move one column at a time, we step 4 values since we have a 4x4 array
+            for (int8_t j = ((columns * rows) - i); j < columns * rows; j -= columns) {
 
-        else if (k - columns < 0) {
-            printf("if where k - columns < 0. k - columns: %d\n", k - columns);
-            return;
-        }
+                if (arr[j] != 0 && arr[j] == arr[j - columns]) {
+                    arr[j] *= 2;
+                    arr[j - columns] = 0;
+                }
 
-        if (arr[k] == arr[k - columns] && arr[k] != 0) {
-            printf("if where arr[k] == arr[k - columns].");
-            printf("k: %d, arr[k] = %d and arr[k - columns] = %d]\n", k, arr[k], arr[k - columns]);
-            arr[k] *= 2;
-            arr[k - columns] = 0;
-            // now we move the values again
-            game_board_recursive_move_values(direction, counter - columns, arr, columns, rows);
+            }
         }
-
-        game_board_recursive_add_pairs(direction, counter - columns, arr, columns, rows);
 
     }
+
+    else if (direction == KEY_LEFT) {
+
+        // we step through rows at a time
+        for (int8_t i = 0; i <= ((columns * rows) - columns); i += columns) {
+            // we move one column at a time
+            for (int8_t j = 0; j < rows - 1; j++) {
+
+                if (arr[i + j] != 0 && arr[i + j] == arr[i + j + 1]) {
+                    arr[i + j] *= 2;
+                    arr[i + j + 1] = 0;
+                }
+            }
+        }
+    }
+
+    else if (direction == KEY_RIGHT) {
+
+        // we step through the rows one at a time, so we step 4 values forward, starting from the right
+        for (int8_t i = rows - 1; i < columns * rows; i += columns) {
+
+            // we start from the right of the game board
+            for (int8_t j = 0; j < rows - 1; j++) {
+
+                if (arr[i - j] != 0 && arr[i - j] == arr[i - j - 1]) {
+                    arr[i - j] *= 2;
+                    arr[i - j - 1] = 0;
+                }
+            }
+        }
+    }
+
+    else {
+
+        printf("oops, there was an error.");
+        return;
+    }
+
+    game_board_tiles_move_direction(direction, arr, columns, rows);
+
 }
+
 
 void game_board_print(int *arr, uint8_t columns, uint8_t rows) {
 
@@ -356,4 +347,10 @@ void game_board_spawn_new_values_random(int *arr, uint8_t columns, uint8_t rows)
         }
     }
 
+}
+
+
+static void game_board_decision_undo(int *arr, int *prev_arr, uint8_t columns, uint8_t rows) {
+
+    memmove(arr, prev_arr, (columns * rows));
 }
